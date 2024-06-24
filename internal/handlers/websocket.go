d9df8742ae2c1e5954aea49076aceb3beb5ab5c1
Shashank <shashanks939@gmail.com>
2024-06-24 22:31:46 +0530

    making the websocket file more better

--- internal/handlers/websocket.go
+++ internal/handlers/websocket.go
@@ -1,10 +1,16 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
+
+// ClientsMap to manage connected clients
+var clients = make(map[*websocket.Conn]bool)
+var clientsMutex sync.Mutex
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
@@ -14,14 +20,43 @@ func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
 	}
 	defer conn.Close()
+
+	// Add the new connection to the clients map
+	clientsMutex.Lock()
+	clients[conn] = true
+	clientsMutex.Unlock()
+
+	log.Println("WebSocket connection established")
 
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			clientsMutex.Lock()
+			delete(clients, conn)
+			clientsMutex.Unlock()
 			break
 		}
-		// Broadcast message to other clients, store in database, etc.
+		log.Printf("Received: %s\n", msg)
+
+		// Broadcast the message to all connected clients
+		broadcastMessage(msg)
+	}
+}
+
+func broadcastMessage(message []byte) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+
+	for client := range clients {
+		err := client.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Printf("Error writing message: %v", err)
+			client.Close()
+			delete(clients, client)
+		}
 	}
 }
